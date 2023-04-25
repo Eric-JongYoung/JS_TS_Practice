83f31d4b8c2de0b6e0050c92ece0739b95700567
Jong <fgh1937@yahoo.com>
2023-04-25 22:16:50 +0900

    ES6 - [03-2] - Arrow Function Practice

--- 02_ES6/step01/02_ArrowFunction/index.js
+++ 02_ES6/step01/02_ArrowFunction/index.js
@@ -26,3 +26,28 @@ var 오브젝트 = {
   }
 }
 오브젝트.함수();
+
+
+// 연습문제
+
+var 사람 = {
+  name : '손흥민',
+  sayHi : function() {
+    console.log('안녕 나는 '+ this.name);
+  }
+}
+사람.sayHi();
+
+var 자료 = {
+  data : [1, 2, 3, 4, 5],
+  전부더하기 : function(){
+    var sum = 0;
+    this.data.forEach(a => sum+=a);
+    console.log(sum);
+  }
+}
+자료.전부더하기();
+
+document.getElementById('버튼').addEventListener('click', function(){
+  setTimeout(()=>{ console.log(this.innerHTML) }, 1000); 
+});
