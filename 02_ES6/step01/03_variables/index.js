e1b0d0c85db5d97f7c1ab4e1e80d4c650218fa95
Jong <fgh1937@yahoo.com>
2023-04-26 23:08:53 +0900

    ES6 - [05] - Variables
    - Hoisting
    - 변수 여러개 한번에 지정
    - 지역번수, 전역변수

--- 02_ES6/step01/03_variables/index.js
+++ 02_ES6/step01/03_variables/index.js
@@ -29,3 +29,64 @@ const 사람 = { 이름 : 'ko'};
 const 인간 = { name : 'hi'};
 Object.freeze(인간);
 인간.name = 'wow';
+
+// Hoisting
+// 변수나 함수의 선언부분을 변수의 범위 맨 위로 강제로 끌고가서 가장 먼저 해석합니다.
+var 몰라;
+
+몰라 = '몰라';
+
+
+
+// 변수 여러개 편리하게 만들기
+let hello = 20, wow = 'wow', nice;
+
+// 전역변수 : 모든곳에서 쓸 수 있는 변수
+
+var 전역변수 = 20;
+
+window.이것도전역변수 = 'ko';
+
+function hola() {
+  console.log(전역변수);
+
+  //지역변수
+  var 지역변수 = 11; 
+}
+
+
+let a = 1;
+var 함수 = function() {
+  a = 2;
+}
+console.log(a); // 1
+
+// 이럴때는 가장 가까운 범위 값으로 변함
+let b = 1;
+var c = 2;
+window.b = 3;
+window.c = 4;
+
+console.log(b + c); // 5
+
+// var i =  5 상태에서 돌기때문에 5만 나오는거임
+// for (var i = 0; i < 5; i++) { 
+//   setTimeout(function() { console.log(i); }, i*1000 ); 
+// }
+
+for (let i = 1; i < 6; i++) { 
+  setTimeout(function() { console.log(i); }, i*1000 ); 
+}
+
+//모달
+var 버튼들 = document.querySelectorAll('button');
+var 모달창들 = document.querySelectorAll('div');
+
+
+// var i 로 되어있으면, var i = 3 이 된 상태로 유지되므로 채워지지가 않음.
+for (let i = 0; i < 3; i++){
+  버튼들[i].addEventListener('click', function(){
+    모달창들[i].style.display = 'block';
+  });
+
+}
