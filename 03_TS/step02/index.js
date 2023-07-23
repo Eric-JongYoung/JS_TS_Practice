6cc9b81c7107a85058356f486b7102086c0231fb
Eric-JongYoung <fgh1937@yahoo.com>
2023-07-23 15:57:45 +0900

    TS - [2] - union , any, unknown

--- 03_TS/step02/index.js
+++ 03_TS/step02/index.js
@@ -15,3 +15,37 @@ var project = {
     days: 30,
     started: true,
 };
+// 타입을 미리 정하기 애매할떄
+// Union Type
+var member = 123;
+member = 'hello';
+var members = [1, '2', 3];
+var obj = { a: '123' };
+// Any - TS를 해제 하고 완전히 JS처럼 쓰는 방법임
+var 아무거나;
+아무거나 = 123;
+아무거나 = true;
+// Unknown - Any랑 비슷하지만 조금더 안전함 
+var 몰라;
+몰라 = 123;
+몰라 = false;
+몰라 = {};
+// let 변수 :string = 몰라; // 다른 변수에 들어가는걸 막아줌
+// 몰라 - 1; // unknown은 number 타입이 아니라서 엄격하게 안됨
+var age;
+// string + 1 => 허용
+// number + 1 => 허용
+// string|number +1 => 안돼
+// age + 1; // Union 타입에는 불가능
+// 연습문제
+var user = 'kim';
+var age1 = undefined;
+var married = false;
+var 철수 = [user, age1, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
--- 03_TS/step02/index.ts
+++ 03_TS/step02/index.ts
@@ -26,3 +26,53 @@ let project :{
   days : 30,
   started : true,
 }
+
+// 타입을 미리 정하기 애매할떄
+
+// Union Type
+let member :(number | string | boolean) = 123;
+member = 'hello';
+
+let members :(number | string)[] = [1,'2',3];
+
+let obj :{ a : string|number} = { a : '123'};
+
+// Any - TS를 해제 하고 완전히 JS처럼 쓰는 방법임
+let 아무거나 :any;
+아무거나 = 123;
+아무거나 = true;
+
+// Unknown - Any랑 비슷하지만 조금더 안전함 
+let 몰라 :unknown;
+몰라 = 123;
+몰라 = false;
+몰라 = {};
+// let 변수 :string = 몰라; // 다른 변수에 들어가는걸 막아줌
+// 몰라 - 1; // unknown은 number 타입이 아니라서 엄격하게 안됨
+
+let age : string|number;
+// string + 1 => 허용
+// number + 1 => 허용
+// string|number +1 => 안돼
+// age + 1; // Union 타입에는 불가능
+
+// 연습문제
+
+let user : string = 'kim';
+let age1 : undefined | number = undefined;
+let married : boolean = false; 
+let 철수 : (string|number|undefined|boolean)[]= [user, age1, married]; 
+
+let 학교 : {
+  score : (number|boolean)[],
+  teacher : string,
+  friend : string | string[]
+}
+= {
+  score : [100, 97, 84],
+  teacher : 'Phil',
+  friend : 'John'
+}
+
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher]
