a1e30a070af253d4fc3aee6931cf549e93d52590
Eric-JongYoung <fgh1937@yahoo.com>
2023-07-23 19:05:11 +0900

    TS - [4] - Type Alias

---
+++ 03_TS/step01/04_Type_Alias/index.js
@@ -0,0 +1,17 @@
+// Type Aliases
+// Type은 낙타표기법 으로 
+// type은 재정의가 불가능하다.
+var 동물 = 'Monkey';
+var user = { name: 'ko', age: 20 };
+var 출생지역 = 'Seoul';
+// 출생지역 = 'Lodnon'; // const는 원래 불변
+// const 변수는 등로로 재할당만 막는 역할임. const로 담은 Object 수정은 자유롭게 가능
+var 출생지역임 = { region: 'seoul' };
+출생지역임.region = 'london';
+var 여친 = {
+    name: 'Nana'
+};
+var 친구 = {
+    name: 'eric'
+};
+var position = { x: 10, y: 20 };
