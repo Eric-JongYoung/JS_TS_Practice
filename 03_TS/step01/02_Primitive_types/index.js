5323660ea443172a022358a2bb182c1bf556868e
MangoEric <fgh1937@yahoo.com>
2024-02-13 23:13:12 +0900

    TS - 02 - Primitive types

---
+++ 03_TS/step01/02_Primitive_types/index.js
@@ -0,0 +1,13 @@
+// string, number, boolean, null, undefined ..
+var 이름 = 'kim';
+이름 = 'lee';
+var 나이 = 20;
+var 결혼유무 = false;
+var 널값 = null;
+var 언디파인드 = undefined;
+var 회원들 = ['kim', 'lee', 'park'];
+var 나이들 = [20, 30, 40];
+var 회원오브젝트 = { member1: 'kim', member2: 'lee' };
+/** 타입지정은 원래 자동으로 됩니다. */
+var 내이름 = 'eric';
+var 내나이 = 20;
---
+++ 03_TS/step01/02_Primitive_types/index.ts
@@ -0,0 +1,21 @@
+
+// string, number, boolean, null, undefined ..
+let 이름 :string = 'kim';
+이름 = 'lee';
+
+let 나이 :number = 20;
+
+let 결혼유무 :boolean = false;
+
+let 널값 :null = null;
+
+let 언디파인드 :undefined = undefined;
+
+let 회원들 :string[] = ['kim', 'lee', 'park'];
+let 나이들 :number[] = [20, 30, 40];
+
+let 회원오브젝트 :{member1 :string, member2 :string} = {member1:'kim', member2:'lee'};
+
+/** 타입지정은 원래 자동으로 됩니다. */ 
+let 내이름 = 'eric';
+let 내나이 = 20;
